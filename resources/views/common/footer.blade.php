8629cfb775654fc2776c7e0a2d63037e5253c56f
Zhang Weian <zhangweian@valuelinkcorp.com>
2020-09-04 10:38:12 +0800

    Copyright 2019改为Copyright 2020

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="page-footer">
-    <div class="page-footer-inner"> © Copyright 2018   |   Claim Gifts Now   |   All Rights Reserved<BR>
+    <div class="page-footer-inner"> © Copyright 2020   |   Claim Gifts Now   |   All Rights Reserved<BR>
         Email: marketing@claimgiftsnow.com<BR>
         <a href="https://www.facebook.com/Claim-Gift-Now-113302237165367/" class="socicon-btn socicon-btn-circle socicon-solid bg-blue font-white bg-hover-grey-salsa socicon-facebook tooltips" data-original-title="Facebook" target="_blank"></a>
         <a href="mailto:marketing@claimgiftsnow.com" class="socicon-btn socicon-btn-circle socicon-solid bg-green font-white bg-hover-grey-salsa socicon-mail tooltips" data-original-title="Mail"></a>
