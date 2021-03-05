5f0e24dae1d020b4f431443c9f796c39d066c3b2
Wulanfang <wulanfang@valuelinkltd.com>
2021-03-05 10:24:31 +0800

    把rsg_product添加的脚本改为1点半执行，并且查的数据节点改为2点

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,12 +14,12 @@ class Controller extends BaseController
 
 	/*
 	 * 得到默认的查询数据日期(产品相关数据)
-	 * 因为VOP系统中addrsgtask脚本数据是早上7点更新，所以在凌晨到早上7点之间显示当天的数据的时候会显示空白
-	 * 因此凌晨到七点半之间要显示的是昨天的数据
+	 * 因为VOP系统中addrsgtask脚本数据是早上1点半更新，所以在凌晨到早上1点半之前显示当天的数据的时候会显示空白
+	 * 因此凌晨到2点之前要显示的是昨天的数据
 	 */
 	public function getDefaultDate($todayDate)
 	{
-		if(time()-strtotime($todayDate.' 02:30:00') < 0){
+		if(time()-strtotime($todayDate.' 02:00:00') < 0){
 			//凌晨到七点半之间要显示的是昨天的数据
 			$todayDate = date('Y-m-d',strtotime($todayDate)-86400);
 		}
